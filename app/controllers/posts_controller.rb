12fe1e6940210f3e702c67e246dec1e5c63742dc
Aguru itabashi <itabashi.a@gmail.com>
2019-03-19 16:39:42 +0900

    sessions_helperのメソッド定義完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,22 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+  end
+
+  def confirm
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
 end
