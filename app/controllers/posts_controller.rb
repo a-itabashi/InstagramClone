069ba2b18518debc12353f3d1c57e5dadd25e8ca
Aguru itabashi <itabashi.a@gmail.com>
2019-03-20 19:39:07 +0900

    投稿一覧の編集・削除ボタンの表示切り替えをした

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :set_params, only: [:show, :edit, :update, :destroy]
+
+
   def index
     @posts = Post.all
   end
@@ -23,7 +26,6 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
     @favorite = current_user.favorites.find_by(post_id: @post.id)
     @favorites = Favorite.where(post_id: @post.id)
   end
@@ -37,12 +39,22 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post.update(post_params)
+    flash[:success] = "投稿が編集されました"
+    redirect_to posts_path
+  end
+
+  def destroy
   end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :content, :post_image, :post_image_cache)
+  end
+
+  def set_params
+     @post = Post.find(params[:id])
   end
 
 end
