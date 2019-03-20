749dd347fdffe9dac6fa1a99fd9781ceaa4274a5
Aguru itabashi <itabashi.a@gmail.com>
2019-03-20 13:40:57 +0900

    posts_controllerのnew,create,confirmアクションを実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,19 +4,37 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    if params[:back]
+      @post = Post.new(post_params)
+    else
+      @post = Post.new
+    end
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "投稿が完了しました"
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+
   end
 
   def confirm
+    @post = current_user.posts.build(post_params)
+    render 'new' if @post.invalid?
   end
 
   def edit
   end
 
   def update
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :post_image, :post_image_cache)
   end
 
 end
