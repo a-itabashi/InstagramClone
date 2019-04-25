8b028cc8d64a76fd67b4830834f4915a8b26be97
Aguru itabashi <itabashi.a@gmail.com>
2019-04-25 15:21:48 +0900

    modify

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,15 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
-      flash[:success] = "ログインしました"
-      redirect_to posts_path
-    else
+    if !user || !user.authenticate(params[:session][:password])
       session.delete(:user_id)
       flash.now[:info] = "もう一度入力し直してください。"
       render 'new'
     end
+    user && user.authenticate(params[:session][:password])
+    session[:user_id] = user.id
+    flash[:success] = "ログインしました"
+    redirect_to posts_path
   end
 
   def destroy
--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,15 @@ module InstagramClone
       # この二行の記述で自動生成しない設定を作成しています。
       g.assets false
       g.helper false
+
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
   end
 end
